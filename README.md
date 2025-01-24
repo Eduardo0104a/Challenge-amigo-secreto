d2248401ed938de405c91be20a1e722ed6c8ba46
Eduardo0104a <eduardo01.04.a@gmail.com>
2025-01-24 12:57:19 -0500

    Terminado

---
+++ README.md
@@ -0,0 +1,26 @@
+# Challenge del Amigo Secreto - *AluraLatam & Oracle*
+
+## Descripción
+
+**Amigo Secreto** es una aplicación web interactiva que permite gestionar un sorteo de amigos secretos. Los usuarios pueden añadir los nombres de sus amigos, realizar un sorteo aleatorio para asignarles un amigo secreto, y mostrar los resultados en tiempo real.
+
+Este proyecto está diseñado para ayudar a gestionar sorteos de amigo secreto de manera sencilla y divertida, sin la necesidad de realizarlo manualmente.
+
+## Funcionalidades
+
+- **Añadir nombres para el sorteo**: Los usuarios pueden agregar los nombres de sus amigos en un campo de texto.
+- **Sorteo aleatorio**: El sistema selecciona aleatoriamente un nombre de la lista de amigos y lo muestra como el amigo secreto.
+
+## Tecnologías
+
+- **HTML5**: Estructura básica de la página web.
+- **CSS3**: Estilización de la página.
+- **JavaScript**: Lógica para gestionar el sorteo de amigos secretos.
+
+## Conéctate conmigo 🌐
+
+¡Me encantaría saber qué piensas sobre este proyecto! Si quieres estar al tanto de más actualizaciones o conectar conmigo, puedes encontrarme en mis redes sociales:
+
+- **[LinkedIn](www.linkedin.com/in/eduardoayalaa)**
+- **[Instagram](https://www.instagram.com/eduardo.nrg/?hl=es-la)**
+- **[Twitter]([www.linkedin.com/in/eduardoayalaa](https://x.com/EduarNGR))**
---
+++ app.js
@@ -0,0 +1,56 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+let amigosSorteados = [];
+
+function agregarAmigo() {
+    let nombre = document.getElementById('amigo').value;
+    if(nombre == ''){
+        alert('Por favor, inserte un nombre');
+        return false;
+    } else if(nombre.match(/\d/)){
+        alert('El nombre no puede contener numeros');
+        return false;
+    } else if(amigos.includes(nombre)){
+        alert('El amigo ya fue ingresado en el sorteo')
+        return false;
+    } else {
+        amigos.push(nombre);
+        recorrerLista();     
+    }
+    console.log(amigos);
+    document.getElementById("amigo").value="";   
+    
+}
+console.log(amigos)
+
+function recorrerLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+    for(let i=0; i<amigos.length;i++){
+        let elemento = document.createElement('li');
+        elemento.textContent = amigos[i];
+        lista.appendChild(elemento);
+    }
+}
+
+function sortearAmigo() {
+    if(amigosSorteados.length==amigos.length){
+        alert('Ya fueron sorteados todos los nombres');
+        amigosSorteados =[];
+        return;
+    }
+    
+    let indiceNombre =0;
+        do {
+            indiceNombre = Math.floor(Math.random() * amigos.length);
+        } while (amigosSorteados.includes(indice)); 
+        document.getElementById('resultado').textContent = `El nombre sorteado es: ${amigos[indiceNombre]}`;
+        amigosSorteados.push(indiceNombre);
+        console.log(amigosSorteados)
+
+    if(amigos.length === 0){
+        alert('No se puede realizar el sorteo');
+        return;
+    }
+    
+}
